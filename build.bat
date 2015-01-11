67d21ec575428b6a3e582f333ecb3f0df1831820
Alex Rabochy <alex.rabochy@gmail.com>
2015-01-11 19:45:24 +0300

    Builed edited

--- build.bat
+++ build.bat
@@ -1,11 +1,11 @@
 @echo off
-if "%running%"=="" goto :EOF
+if "%start%"=="" goto :EOF
 
-%MSBuild%\MSBuild.exe %RepositoryPath%\%ProjectName%\%Solution% /p:Configuration=Release;VisualStudioVersion=13.0 > %BuildLog%
+%MSBuild%\MSBuild.exe %RepositoryPath%\%ProjectName%\%Solution% /p:Configuration=Release;VisualStudioVersion=13.0 > %MSBuildLog%
 if ERRORLEVEL 1 set Builded=false
 
-for /F %%file in %FileList% do (
-	if not exist %BuildPath%\%%file (
-		set Builded=false
-	)
-) 
+for /F %%F in ( %FileList% ) do (
+    if not exist %BuildPath%\%%F (
+        set Builded=false
+    )
+)
--- settings.bat
+++ settings.bat
@@ -1,12 +1,12 @@
 @echo off
 if "%start%"=="" goto :EOF
 
-set Git="C:\Program Files\Git\bin"
-set MSBuild="C:\Windows\Microsoft.NET\Framework\v4.0.30319"
-set Blat="C:\Blat"
-set NUnit="C:\Program Files\NUnit-2.6.4\bin"
+set Git=C:\Program Files\Git\bin
+set MSBuild=C:\Windows\Microsoft.NET\Framework\v4.0.30319
+set Blat=C:\Blat
+set NUnit=C:\Program Files\NUnit-2.6.4\bin
 
-set RepositoryPath="C:\GitHub"
+set RepositoryPath=C:\GitHub
 set ProjectName=Geometry
 set RepositoryURL=https://github.com/kazimure/%ProjectName%
 
@@ -20,21 +20,7 @@ set TestOut=test.txt
 set FileList=files.txt
 
 set Solution=Geometry.sln
-set BuildPath="%RepositoryPath%\%ProjectName%\Geometry\bin\Release
-set TestPath="%RepositoryPath%\%ProjectName%\Tests\bin\Release
+set BuildPath="%RepositoryPath%\%ProjectName%\Geometry\bin\Release"
+set TestPath="%RepositoryPath%\%ProjectName%\Tests\bin\Release"
 
 set MailTo=alex.rabochy@gmail.com
-
-
-
-
-
-
-
-
-
-
-set BuildLocation="%RepositoryPath%\%RepositoryName%\UI\bin\Release"
-set TestLocation="%RepositoryPath%\%RepositoryName%\Tests\bin\Release"
-
-set EmailReceiver=anastasia.sulyagina@gmail.com
